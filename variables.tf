d8be317abd9b563b2980d67d50057be637b73c59
Kunal Malhotra <kunalmalhotra@Kunals-MacBook-Pro.local>
2020-01-07 10:53:26 +0400

    Changes to the variable.tf file

--- variables.tf
+++ variables.tf
@@ -1,19 +1,18 @@
-#Required Varianbles
-# variable "subscription_id" {
-#   description = "The Azure Subscription Id - found in the azure portal"
-# }
+variable "subscription_id" {
+   description = "The Azure Subscription Id - found in the azure portal"
+}
 
-# variable "client_id" {
-#   description = "The Client Id assigned when adding the application to the authorizing Azure Active Directory Account"
-# }
+variable "client_id" {
+  description = "The Client Id assigned when adding the application to the authorizing Azure Active Directory Account"
+}
 
-# variable "client_secret" {
-#   description = "The Client Secret assigned in Azure Active Directory when adding a key"
-# }
+variable "client_secret" {
+   description = "The Client Secret assigned in Azure Active Directory when adding a key"
+}
 
-# variable "tenant_id" {
-#   description = "The Guid assigned to the application in Azure Active Directory"
-# }
+variable "tenant_id" {
+   description = "The Guid assigned to the application in Azure Active Directory"
+}
 
 variable "username" {
   description = "Virtual Machine User Name"
@@ -23,7 +22,6 @@ variable "password" {
   description = "Virtual Machine Password"
 }
 
-# Optional Overrides
 variable "region" {
   description = "The Azure region the resources should be created in"
   default     = "eastus"
